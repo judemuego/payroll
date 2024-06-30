73f5ccf773a853ecc79628a7ce2d2c3093566c2d
Arvin Olivas <arvin.olivas15@gmail.com>
2024-07-01 01:13:37 +0800

    User Access

--- resources/views/backend/pages/setup/role.blade.php
+++ resources/views/backend/pages/setup/role.blade.php
@@ -3,32 +3,21 @@
 @section('title', 'Role')
 
 @section('breadcrumbs')
-    <span>Account</span> / <span class="highlight">Role</span>
+    <span>Setup</span>  / <span>User Access</span> / <span class="highlight">Role</span>
 @endsection
 
 @section('content')
-
 <div class="row">
     <div class="col-md-12">
         <div class="card">
             <div class="card-header">
-                <h5 class="card-title">Role Screen
-                    <button type="button" class="btn btn-primary add" data-toggle="modal" data-target="#classModal" style="float:right">
-                        Add Role
-                    </button>
+                <h5 class="card-title">Role
                 </h5>
             </div>
             @include('backend.partial.flash-message')
             <div class="col-12">
                 <div class="card-body">
-                    <table id="datatables" class="table table-striped" style="width:100%">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Action</th>
-                                <th>Role Name</th>
-                            </tr>
-                        </thead>
+                    <table id="role_table" class="table table-striped" style="width:100%">
                     </table>
                 </div>
             </div>
@@ -36,127 +25,31 @@
     </div>
 </div>
 
-{{-- MODAL --}}
-<div class="modal fade" id="classModal" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <form id="save_record" method="post" enctype="multipart/form-data">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Add Role</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body m-3">
-                        @csrf
+@section('sc-modal')
+@parent
+<div class="sc-modal-content" id="role_form">
+    <div class="sc-modal-dialog">
+        <div class="sc-modal-header">
+            <span class="sc-title-bar"></span>
+            <span class="sc-close" onclick="scion.create.sc_modal('role_form').hide('all', modalHideFunction)"><i class="fas fa-times"></i></span>
+        </div>
+        <div class="sc-modal-body">
+            <form id="roleForm" method="post" class="form-record">
+                <div class="row">
                     <div class="form-group col-md-12">
-                        <label for="">Role Name</label>
-                        <input type="text" class="form-control" id="name" name="name" placeholder="Enter Role Name" required/>
+                        <label for="">Name</label>
+                        <input type="text" class="form-control" id="name" name="name">
                     </div>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary submit-button">Add</button>
-                </div>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
 </div>
-{{-- MODAL --}}
-                    
-                    
-    @stop
+@endsection
+    
+@endsection
 
-    @section('scripts')
-    <script src="//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-    <script>
-        var deleteClasses=null;
-        $(function() {
-            $('#datatables').DataTable({
-                responsive: true, 
-                processing: true,
-                serverSide: true,
-                ajax: {
-                    url: "/settings/role/get",
-                    type: "GET"
-                },
-                columns: [
-                    { data: "DT_RowIndex", title:"#" },
-                    { data: "id", title:"Action", render: function(data, type, row, meta) {
-                        var html = "";
-                        html += '<a href="#" class="align-middle edit" onclick="edit(' + row.id + ')" title="Edit" data-toggle="modal" data-target="#classModal"><i class="fas fa-pen"></i></a>';
-                        html += '<a href="#" onclick="deleteRecord(' + row.id+ ')"><i class="align-middle fas fa-fw fa-trash"></i></a>';
-                        return html;
-                    }},
-                    { data: "name", title: "Role Name" }
-                ]
-            });
-
-            $('.add').click(function(){
-                $('.modal-title').text('Add Role');
-                $('.submit-button').text('Add');
-                $('#save_record')[0].reset();
-                record_id = null;
-            });
-
-            $('#save_record').validate({
-                submitHandler: function (form) {
-                    var formData = $("#save_record").serialize();
-
-                    if(record_id === null) {
-                        scion.record.add('/settings/role/save', formData,
-                        function() {
-                            $('#datatables').DataTable().draw();
-                            $('#classModal').modal('hide');
-                        });
-                    }
-                    else {
-                        scion.record.update('/settings/role/update', record_id, formData,
-                        function() {
-                            $('#datatables').DataTable().draw();
-                            $('#classModal').modal('hide');
-                        },
-                        function() {});
-                    }
-
-                    return false;
-                }
-            });
-        });
-
-        function edit(id){
-            event.preventDefault();
-            $.ajax({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                url: '/settings/role/edit/' + id,
-                method: 'get',
-                data: {},
-                success: function(data) {
-                    $('.modal-title').text('Update Role');
-                    $('.submit-button').text('Update');
-                    $('#save_record')[0].reset();
-                    record_id = id;
-
-                    $.each(data, function() {
-                        $.each(this, function(k, v) {
-                            $('#'+k).val(v);
-                        });
-                    });
-                }
-            });
-        }
-
-        function deleteRecord(id) {
-            delete_func = scion.record.delete('/settings/role/destroy', id,
-            function() {
-                $('#datatables').DataTable().draw();
-            },
-            function() {
-                console.log('error');
-            });
-        }
-        
-    </script>
+@section('scripts')
+<script src="//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script src="/js/backend/pages/setup/role.js"></script>
 @endsection
